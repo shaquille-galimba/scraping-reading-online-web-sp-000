0ec74b4dd549599c48cfdda8bf31a7ec2bd896ea
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-29 08:32:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,4 +6,4 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 
 # puts doc.css(".headline-26OIBN").text.strip
-puts doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .title-oE5vT4")
+print doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .title-oE5vT4")
