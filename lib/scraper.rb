8d6956f120de0f27768f0eca33af7ffd68d015ad
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-29 09:12:45 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,10 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+# puts doc.css(".headline-26OIBN").text.strip
+courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .title-oE5vT4")
+p courses[0].attributes
+# courses.each {|course| puts course.text.strip}
